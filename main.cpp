53d01d8c6f523733eb80ea1aa2b8de002d47947b
endurovojta173 <vojtechbrenek173@gmail.com>
2024-09-29 11:05:53 +0200

    Input of second number
    I have added input for number2

--- main.cpp
+++ main.cpp
@@ -4,10 +4,17 @@ int main()
 {
     std::cout << "Hello world";
 
-    std::cout << "\nInput one number: ";
+    std::cout << "\nInput first number: ";
 
     int number1 = 0;
-    std::cout<<"Your number: " << number1;
-    getchar();  
+    int number2 = 0;
+    std::cin >> number1;
+    std::cout<<"Your first number: " << number1;
+    std::cout << "\nInput second number: ";
+    std::cin>>number2;
+    std::cout<<"Your second number: " << number2;
+
+    getchar();
+    getchar();
     return 0;
 }
